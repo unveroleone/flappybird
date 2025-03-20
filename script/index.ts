1c9257784499810680ef43376640e05390f37bbd
Zielke Ramon <Ramon.Zielke@swisscom.com>
2025-03-20 11:19:49 +0100

    made ts code more understandeable

--- script/index.ts
+++ script/index.ts
@@ -1,7 +1,7 @@
 let gravity: number = 9.81; 
 let velocity: number = 0;    
 let positionY: number = 100; // start height in px
-let positionX : number = 0; // start position in px
+let positionXGround : number = 0; // start position in px
 let speed : number = -10;
 let deltaTime: number = 0.1; 
 let jumpForce : number = 50;
@@ -12,11 +12,12 @@ const player = document.getElementById("player")!;
 const ground1 = document.getElementById("ground1")!;
 const ground2 = document.getElementById("ground2")!;
 const ground3 = document.getElementById("ground3")!;
+const pipes = document.getElementsByClassName("pipe");
 
 function update(): void {
     velocity += gravity * deltaTime;
     positionY += velocity * deltaTime;
-    positionX += speed * deltaTime;
+    positionXGround += speed * deltaTime;
 
     if (positionY >= groundLevel) {
         positionY = groundLevel; // stopps falling
@@ -31,13 +32,13 @@ function update(): void {
         clearInterval(gameLoop);
     }
 
-    if(positionX <= -ground1.offsetWidth){
-        positionX = 0;
+    if(positionXGround <= -ground1.offsetWidth){
+        positionXGround = 0;
     }
     player.style.top = `${positionY}px`; 
-    ground1.style.left = `${positionX}px`;
-    ground2.style.left = `${positionX + ground1.offsetWidth}px`;
-    ground3.style.left = `${positionX + ground1.offsetWidth}px`;
+    ground1.style.left = `${positionXGround}px`;
+    ground2.style.left = `${positionXGround + ground1.offsetWidth}px`;
+    ground3.style.left = `${positionXGround + ground1.offsetWidth}px`;
     console.log(`Position: ${positionY.toFixed(2)}px, Geschwindigkeit: ${velocity.toFixed(2)}px/s`);
 }
 
@@ -52,3 +53,17 @@ document.addEventListener("keydown", (event) => {
 });
 
 let gameLoop = setInterval(update, deltaTime * 100);
+
+function isColliding(el1: HTMLElement, el2: HTMLElement): boolean {
+    if (!el1 || !el2) return false;
+
+    const rect1 = el1.getBoundingClientRect();
+    const rect2 = el2.getBoundingClientRect();
+
+    return (
+        rect1.left < rect2.right &&
+        rect1.right > rect2.left &&
+        rect1.top < rect2.bottom &&
+        rect1.bottom > rect2.top
+    );
+}
